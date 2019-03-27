ba749d56a45d8242cdfa3c6282c9078fe3cd10b5
Saerom Bang <saerombang11@gmail.com>
2019-03-27 15:27:33 +0900

    added coding challenge 5

--- script.js
+++ script.js
@@ -506,70 +506,75 @@ for (var i = john.length -1; i >= 0; i--){
 }
 */
 
+/******** 
 var john = {
     fullName: 'John Smith',
     bills: [124, 48, 268, 180, 42],
-    clacTips: function() {
+    calcTips: function() {
         this.tips = [];
         this.finalValues = [];
-
-        for (var i = 0; i < this.bills.length; i++)
-        {
-            // Detemine percentage based on tipping rules
+                
+        for (var i = 0; i < this.bills.length; i++) {
+            // Determine percentage based on tipping rules
             var percentage;
             var bill = this.bills[i];
-
+            
             if (bill < 50) {
                 percentage = .2;
-            } else if (bill >= 50 && bill < 200)  {
+            } else if (bill >= 50 && bill < 200) {
                 percentage = .15;
             } else {
                 percentage = .1;
             }
-
+            
+            // Add results to the corresponing arrays
             this.tips[i] = bill * percentage;
             this.finalValues[i] = bill + bill * percentage;
         }
     }
 }
-
-john.clacTips();
-console.log(john);
-
 var mark = {
     fullName: 'Mark Miller',
-    bills: [77, 478, 110, 45],
-    calcTips: function(){
+    bills: [77, 475, 110, 45],
+    calcTips: function() {
         this.tips = [];
         this.finalValues = [];
-
-        for (var i = 0; i < this.bills.length; i++)
-        {
+                
+        for (var i = 0; i < this.bills.length; i++) {
+            // Determine percentage based on tipping rules
             var percentage;
             var bill = this.bills[i];
-
+            
             if (bill < 100) {
                 percentage = .2;
-            } else if (bill >= 100 && bill < 300){
+            } else if (bill >= 100 && bill < 300) {
                 percentage = .1;
             } else {
-                percentage = .25
+                percentage = .25;
             }
-
+            
+            // Add results to the corresponing arrays
             this.tips[i] = bill * percentage;
             this.finalValues[i] = bill + bill * percentage;
         }
     }
 }
-
-function clacAverage(tips){
+function calcAverage(tips) {
     var sum = 0;
-    for(var i = 0; i < tips.length; i++) {
+    for (var i = 0; i < tips.length; i++) {
         sum = sum + tips[i];
     }
+    return sum / tips.length;
 }
-// [2, 6, 4] -> 0/2/8/12
-
+// Do the calculations
 john.calcTips();
 mark.calcTips();
-console.log(john. mark);
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+if (john.average > mark.average) {
+    console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+} else if (mark.average > john.average) {
+    console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+}
+*/
